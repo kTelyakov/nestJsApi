dbbbedb1f8426d945afc44a8ede2ca92055e0600
Konstantin <telyakovkostya.learn@gmail.com>
2021-11-27 02:45:32 +0300

    Db connect

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
 import { Module } from '@nestjs/common';
+import {SequelizeModule} from '@nestjs/sequelize'
 
  @Module({
   controllers: [AppController],
-  providers: [AppService]
+  providers: [AppService],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'konstantin',
+      password: 'Qq123123',
+      database: 'nest-course',
+      models: [],
+      autoLoadModels: true
+    }),
+  ]
  })
  export class AppModule {
 
