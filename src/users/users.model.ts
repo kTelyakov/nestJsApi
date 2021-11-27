3192138e61d17096a9409914959d19055058cebd
Konstantin <telyakovkostya.learn@gmail.com>
2021-11-27 04:45:53 +0300

    User roles table created

--- src/users/users.model.ts
+++ src/users/users.model.ts
@@ -1,5 +1,6 @@
-import {Column, DataType, Model, Table} from "sequelize-typescript";
+import {BelongsToMany, Column, DataType, Model, Table} from "sequelize-typescript";
 import {ApiProperty} from "@nestjs/swagger";
+import {UserRoles} from "../roles/user-roles.model";
 
 interface UserCreationAttrs {
   email: string
@@ -31,4 +32,7 @@ export class User extends Model<User, UserCreationAttrs> {
   @ApiProperty({ example: "спамил!", description: 'Причина бана' })
   @Column({ type: DataType.STRING, allowNull: true })
   banReason: string
+
+  @BelongsToMany(() => User, () => UserRoles)
+  users: User[]
 }
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,12 +3,14 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import {SequelizeModule} from "@nestjs/sequelize";
 import {User} from "./users.model";
+import {Role} from "../roles/roles.model";
+import {UserRoles} from "../roles/user-roles.model";
 
 @Module({
   providers: [UsersService],
   controllers: [UsersController],
   imports: [
-    SequelizeModule.forFeature([User])
+    SequelizeModule.forFeature([User, UserRoles])
   ]
 })
 export class UsersModule {}
