dc843dd443b1141ada2797cd7eb24e0e8b29a5ea
Konstantin <telyakovkostya.learn@gmail.com>
2021-11-27 03:41:42 +0300

    Added swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { AppModule } from './app.module';
 import { NestFactory } from '@nestjs/core';
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
 
 async function start () {
   const PORT = process.env.PORT || 5000
   const app = await NestFactory.create(AppModule)
+
+  const config = new DocumentBuilder()
+    .setTitle('Урок по nest')
+    .setDescription('ДОКА ЕБАНА!')
+    .setVersion('111')
+    .addTag('FUCK')
+    .build()
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/api/docs', app, document)
 
   await app.listen(PORT, () => console.log(`Start on ${PORT}`))
 }
