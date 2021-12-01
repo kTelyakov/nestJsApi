5e836af515f317ad99b80fb1589d18e74aef7f61
Konstantin <telyakovkostya.learn@gmail.com>
2021-12-01 22:59:17 +0300

    remove all shit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,6 @@
 import { Module } from '@nestjs/common';
 import {SequelizeModule} from '@nestjs/sequelize'
-import { UsersModule } from './users/users.module';
 import {ConfigModule} from '@nestjs/config'
-import {User} from "./users/users.model";
-import { RolesModule } from './roles/roles.module';
-import {Role} from "./roles/roles.model";
-import {UserRoles} from "./roles/user-roles.model";
-import { AuthModule } from './auth/auth.module';
 
  @Module({
   controllers: [],
@@ -22,12 +16,9 @@ import { AuthModule } from './auth/auth.module';
       username: process.env.POSTGRESS_USER,
       password: process.env.POSTGRESS_PASSWORD,
       database: process.env.POSTGRESS_DB,
-      models: [User, Role, UserRoles],
+      models: [],
       autoLoadModels: true
-    }),
-    UsersModule,
-    RolesModule,
-    AuthModule,
+    })
   ]
  })
  export class AppModule {
